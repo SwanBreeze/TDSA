072e1476a5c8a6f0f0ef5cba15ba47e0cbb66ad1
razaneSi <232337389717@etu.univ-usto.dz>
2025-05-09 13:46:58 +0100

    Update service_worker_cached.js

--- service_worker_cached.js
+++ service_worker_cached.js
@@ -1,4 +1,4 @@
-const cacheName = 'v1';
+const cacheName = 'v2'; 
 
 const cachedAssets = [
     'sidePanel.html',
@@ -18,49 +18,53 @@ const cachedAssets = [
     'PDF.png',
     'DOCX.png',
     'EXCEL.png'
-    
-]
+];
 
-//install
-self.addEventListener('install', event =>{
-    console.log('Service Worker: Installed')
-
+// install
+self.addEventListener('install', event => {
+    console.log('Service Worker: Installed');
 
     event.waitUntil(
-        caches
-        .open(cacheName)
-        .then((cache) =>{
-            console.log('Service Worker: Caching Files')
-            cache.addAll(cachedAssets);
-        })
-        .then(()=> self.skipWaiting())
+        caches.open(cacheName).then(cache => {
+            console.log('Service Worker: Caching Files');
+            return cache.addAll(cachedAssets);
+        }).then(() => self.skipWaiting())
     );
 });
 
-//activate
-self.addEventListener('activate', event =>{
+// activate
+self.addEventListener('activate', event => {
     console.log('Service Worker: Activated');
+
+    // to remove old caches
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(name => {
+                    if (name !== cacheName) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(name);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
 });
 
-self.addEventListener('fetch', event =>{
-    console.log('Service Worker: Fetching');
+// fetch
+self.addEventListener('fetch', event => {
+    console.log('Service Worker: Fetching', event.request.url);
 
-    event.respondWith(fetch(event.request)
-     .catch(()=>{
-       return caches.match(event.request)
-     })
-)
-})
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
 
-
-
-
-/*for notification*/
-
+// push notification
 self.addEventListener('push', event => {
     const data = event.data.json();
     self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: 'android-chrome-192.png',
+        body: data.body,
+        icon: 'search_icon_cropped_192x192.png',
     });
-  });
+});
